db486ad81401a36a647e5039f8d21477141c2aae
Alexander Leris <alexander.leris@gmail.com>
2021-05-02 22:37:17 +1000

    fix naming and import error

--- phase1.c
+++ phase1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
 
 int main(int argc, char* argv[]) {
     /* read in the length of the message and fix byte-ordering */
@@ -8,7 +11,7 @@ int main(int argc, char* argv[]) {
     if (fread(&message_length, sizeof(uint16_t), 1, stdin) != 1) {
         exit(EXIT_FAILURE);
     }
-    message_length = nthos(message_length);
+    message_length = ntohs(message_length);
 
     printf("scan in length %" PRId16 "\n", message_length);
 
