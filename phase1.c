4f072bd6827f7ac7d6d698254559d1d9fef91b3a
Alexander Leris <alexander.leris@gmail.com>
2021-05-02 22:11:46 +1000

    add read in of message length

--- phase1.c
+++ phase1.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
 int main(int argc, char* argv[]) {
+    /* read in the length of the message and fix byte-ordering */
+    uint16_t message_length;
+    if (fread(&message_length, sizeof(uint16_t), 1, stdin) != 1) {
+        exit(EXIT_FAILURE);
+    }
+    message_length = nthos(message_length);
+
+    printf("scan in length %" PRId16 "\n", message_length);
+
     return 0;
 }
