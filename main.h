b5574acca0aae7b25919778e0fb79662133418e1
Alexander Leris <alexander.leris@gmail.com>
2021-05-12 20:48:31 +1000

    get server code from lab9, change port number

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define _POSIX_C_SOURCE 200112L
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "dns.h"
+
+#define QUERY_PORT_NO  8053
+#define QUERY_PORT_STR "8053"
+
+#define ACCEPTED_QTYPE TYPE_AAAA
+
+#endif
