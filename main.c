ecc1e79075968501fca36e0ec54fca4fea05ec75
Alexander Leris <alexander.leris@gmail.com>
2021-05-20 14:56:23 +1000

    add parsing of dns response

--- main.c
+++ main.c
@@ -33,7 +33,9 @@ int main(int argc, char* argv[]) {
 			response_len    = request_len;
 		} else {
 			/* AAAA request is made, forward along to the upstream server */
+			struct dns_message dns_response = {0};
 			response_len = dns_upstream_connection(argv[1], argv[2], &response_buffer, request_buffer, request_len);
+			response_len = parse_request(&dns_response, &response_buffer, response_len);
 		}
 
 		/* return the packet back to the client over that original connection */
